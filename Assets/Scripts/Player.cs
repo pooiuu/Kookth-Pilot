99310a11f84b97b87a74ffa194ef527a1f8d1939
= <jsm1505104@gmail.com>
2017-11-16 15:56:58 +0900

    Script Refactoring, 2인 로컬 플레이 구현

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerInfo
+{
+	public int index = 0;
+	public float hp = 100;
+
+	public bool isDead = false;
+}
+[System.Serializable]
+public class EngineInfo {
+	public float power = 100.0f;
+	public Transform engine1;
+	public Transform engine2;
+}
+public class Player : MonoBehaviour {
+	public static int localIndex = 0;
+	public PlayerInfo pInfo;
+	public EngineInfo eInfo;
+	private Rigidbody2D rb;
+
+	public event NoParameter OnDamaged;
+	public event NoParameter OnDead;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+
+	public void OnCollisionEnter2D (Collision2D col)
+	{
+		Damaged ();
+	}
+
+	public void OnCollisionStay2D (Collision2D col)
+	{
+		Damaged ();
+	}
+
+	public static void SetLocalIndex (int index)
+	{
+		localIndex = index;
+	}
+
+	public void Damaged ()
+	{
+		float damage = Mathf.Max(1f, rb.mass * rb.velocity.magnitude);
+		pInfo.hp = Mathf.Max (0f, pInfo.hp - damage);
+			
+		if (null != OnDamaged)
+			OnDamaged.Invoke();
+		
+		if (pInfo.hp <= 0) {
+			pInfo.isDead = true;
+			if (null != OnDead)
+				OnDead.Invoke ();
+		}
+	}
+
+	public void UseLeftEngine ()
+	{
+		rb.AddForceAtPosition(eInfo.power * eInfo.engine1.up, eInfo.engine1.position);
+	}
+	public void UseRightEngine ()
+	{
+		rb.AddForceAtPosition(eInfo.power * eInfo.engine2.up, eInfo.engine2.position);
+	}
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+	public List<Player> localPlayer;
+	public bool inputA;
+	public bool inputD;
+	// 2인 플레이가 아닌 P2P 대전이 될때는 아래 스크립트를 지워도 됨
+	public bool inputArrow_Left;
+	public bool inputArrow_Right;
+
+	// Use this for initialization
+	void Start () {
+		Player[] ps = GameObject.FindObjectsOfType<Player> ();
+		foreach(Player p in ps)
+		{
+			if (p.pInfo.index == Player.localIndex)
+				localPlayer.Add (p);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		inputA = Input.GetKey (KeyCode.A);
+		inputD = Input.GetKey (KeyCode.D);
+		// 2인 플레이가 아닌 P2P 대전이 될때는 아래 스크립트를 지워도 됨
+		inputArrow_Left = Input.GetKey (KeyCode.LeftArrow);
+		inputArrow_Right = Input.GetKey (KeyCode.RightArrow);
+	}
+	private void FixedUpdate ()
+	{
+		if (localPlayer.Count < 1) // Local Player가 1명 이상일 때만 인풋을 받음
+			return;
+		if (inputA) {
+			localPlayer[0].UseLeftEngine ();
+		}
+		if (inputD) {
+			localPlayer [0].UseRightEngine ();
+		}
+		// 2인 플레이가 아닌 P2P 대전이 될때는 아래 스크립트를 지워도 됨
+		if (localPlayer.Count < 2) // Local Player가 2명 이상일 때만 인풋을 받음
+			return;
+		if (inputArrow_Left) {
+			localPlayer [1].UseLeftEngine ();
+		}
+		if (inputArrow_Right) {
+			localPlayer [1].UseRightEngine ();
+		}
+	}
+}
