d66f2fc43e60b92008dcade91c05782947de350d
DaeHyun Kang <limitedcopy@gmail.com>
2017-11-18 21:58:26 +0900

    카메라 제어, 장애물 제어 추가

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -4,16 +4,28 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour {
     public GameObject Player;
+    public MapGenerate MG;
+    public float mapWidth;
 	// Use this for initialization
 	void Start () {
-		
+        MG = GameObject.Find("Mapgenerator").GetComponent<MapGenerate>();
+        mapWidth = MG.mapWidth;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Player != null)
+        if (Player != null)
         {
             Camera.main.transform.position = new Vector3(Player.transform.position.x, Camera.main.transform.position.y, Camera.main.transform.position.z);
+
+            if (Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0)).x < (MG.instFloor[0].transform.position.x - mapWidth / 2))
+            {
+                Camera.main.transform.position = new Vector3((MG.instFloor[0].transform.position.x - mapWidth / 2) + MG.viewWidth / 2, Camera.main.transform.position.y, Camera.main.transform.position.z);
+            }
+            else if (Camera.main.ViewportToWorldPoint(new Vector3(1, 0, 0)).x > (MG.instFloor[0].transform.position.x + mapWidth / 2))
+            {
+                Camera.main.transform.position = new Vector3((MG.instFloor[0].transform.position.x + mapWidth / 2) - MG.viewWidth / 2, Camera.main.transform.position.y, Camera.main.transform.position.z);
+            }
         }
 	}
 }
--- Assets/Scripts/MapGenerate.cs
+++ Assets/Scripts/MapGenerate.cs
@@ -9,82 +9,114 @@ public class MapGenerate : MonoBehaviour {
     public GameObject pillarPrefab;//기둥 prefab
     public GameObject floor;//지붕과 바닥 prefab
     public GameObject Goal; //결승선 prefab
-    public float floorHeight;//지붕과 바닥 prefab의 높이
 
     //뷰포트 크기에 따른 맵의 높이와 너비
     public float viewWidth;
     public float viewHeight;
 
+    //맵의 넓이 = 천장(바닥)의 길이(Camera 무빙에 사용)
+    public float mapWidth;
+
     //랜덤 맵 제너레이터 관련 변수들
-    public int pillarsNum = 10;
-    public float pillarSpace = 5;
-    
+    public int pillarsNum = 10; //생성할 기둥 쌍(위 아래)의 개수 
+    public float pillarSpace = 5; //기둥이 나타나는 간격
+
+    //장애물 기둥의 포지션. createPillar에서 다룹니다.
+    public Vector3 pillarPos;
+
     //지붕과 바닥의 위치
     Vector3 roofPos;
     Vector3 floorPos;
 
-    GameObject[] instFloor;
+
+    public GameObject[] instFloor;
 
 
     //랜덤 맵을 생성하는 함수(Start 문에서 사용합니다)
     void createPillar()
     {
-        
-        Vector3 startPos = Camera.main.ViewportToWorldPoint(new Vector3(1, 0.5f, 0));//기둥 시작 position. 기둥 시작은 viewport 오른쪽 끝자락부터.
-        Vector3 pillarPos = new Vector3(startPos.x,startPos.y,0);
 
+        //기둥 시작 position. 기둥 시작은 viewport 오른쪽 끝자락부터. 현재 Viewport의 좌표를 월드 좌표계로 받아옵니다.
+        Vector3 startPos = Camera.main.ViewportToWorldPoint(new Vector3(1, 0.5f, 0));
+        //기둥의 첫 시작 position은 Viewport의 오른쪽 끝입니다.(1, 0.5) 좌표 > Viewport의 X축 오른쪽 끝, Y축 상 가운데.
+        pillarPos = new Vector3(startPos.x,startPos.y,0);
+
+        //위 아래 기둥을 저장할 배열입니다.
         GameObject[] tempPillar = new GameObject[2];
+
+        GameObject tempGoal;
+
+        //플레이어(비행물체)의 Width 값을 받아옵니다. 이는 플레이어의 콜라이더(BoxCollider2D)의 x 너비를 받아옵니다.
         float playerSize = PlayerPrefab.GetComponent<Collider2D>().bounds.size.x;
+        //기둥의 너비를 Random 값으로 받기 위해 선언한 변수입니다.
         float pillarWidth;
+        //위 아래 기둥 사이의 간격을 Random 값으로 받기 위해 선언한 변수입니다.
         float gapSize;
 
+        //어떤 화면에서든 맵 생성이 잘 될 수 있게 하기 위해 View의 높이를 구합니다. Viewport의 Y축 최소에서 최대까지의 World 좌표계를 구하여 계산합니다.
         viewHeight = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0)).y;
+        //장애물 기둥의 기본 길이는 ViewHeight입니다. X크기를 0으로 둔 이유는 후에 pillarWidth를 이용하여 Random값으로 받기 위함입니다.
         pillarPrefab.transform.localScale = new Vector3(0, viewHeight, 1);
+
+
 
         for (int i=0; i<pillarsNum; i++)
         {
+            //기둥의 너비를 Random으로 받음 (PlayerSize 는 현재 플레이어(비행물체)의 Width 값을 받았습니다. Random.range 를 이용하여 Width값의 1/2에서 X2 까지의 값 중에서 랜덤하게 받아옵니다.)
             pillarWidth = Random.Range(playerSize / 2, playerSize * 2);
+            //기둥 사이의 간격을 Random으로 받음 (역시 PlayerSize 를 이용하여 Random 값을 받습니다.)
             gapSize = Random.Range(playerSize, playerSize * 2);
             for (int j = 0; j < 2; j++)
             {
+                //기둥을 생성합니다.
                 tempPillar[j] = Instantiate(pillarPrefab, pillarPos, Quaternion.identity);
+                //기둥들의 Parent는 Mapgenrator 코드를 지니고 있는 객체로 설정됩니다.
                 tempPillar[j].transform.parent = transform;
+                //Random 값으로 받은 pillarWidth를 localScale에 더하여 기둥의 Width를 설정합니다.
                 tempPillar[j].transform.localScale += new Vector3(pillarWidth, 0, 0);
 
             }
+            //맵의 높이의 1/2(= 중간) + gapsize를 위 아래 기둥에 각각 적용합니다. gapsize만큼 위 아래 기둥의 간격을 벌립니다.
             tempPillar[0].transform.position += new Vector3(0, viewHeight / 2 + gapSize / 2, 0);
             tempPillar[1].transform.position += new Vector3(0, -(viewHeight / 2 + gapSize / 2), 0);
+            //
             pillarPos += new Vector3(pillarSpace, 0, 0);
         }
+        //마지막으로 골인 지점을 생성합니다.
+        tempGoal = Instantiate(Goal, pillarPos, Quaternion.identity);
+        tempGoal.transform.localScale = new Vector3(1,viewHeight,0);
+
+
     }
 
     // Use this for initialization
     void Start () {
         instFloor = new GameObject[2];
-        floorHeight = floor.GetComponent<BoxCollider2D>().bounds.size.y;
+
 
         viewWidth = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, 0)).x - Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0)).x;
 
+        createPillar();
         //천장과 바닥 위치와 크기 설정
-        roofPos = new Vector3(Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0, 0)).x, Camera.main.ViewportToWorldPoint(new Vector3(0,1,0)).y, 0);
-        floorPos = new Vector3(Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0, 0)).x, Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0)).y, 0);
+        roofPos = new Vector3(pillarPos.x / 2, Camera.main.ViewportToWorldPoint(new Vector3(0,1,0)).y, 0);
+        floorPos = new Vector3(pillarPos.x / 2, Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0)).y, 0);
         instFloor[0] = Instantiate(floor, roofPos, Quaternion.identity);
         instFloor[1] = Instantiate(floor, floorPos, Quaternion.identity);
 
         for (int i = 0; i < 2; i++)
         {
             Vector3 tempScale = instFloor[i].transform.localScale;
-            instFloor[i].transform.localScale = new Vector3(viewWidth, tempScale.y, tempScale.z);
+            instFloor[i].transform.localScale = new Vector3(viewWidth + pillarPos.x, tempScale.y, tempScale.z);
+           
         }
+        mapWidth = viewWidth + pillarPos.x;
 
-        createPillar();
 
     }
 	
 	// Update is called once per frame
 	void Update () {
-        instFloor[0].transform.position = new Vector3(Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0, 0)).x, Camera.main.ViewportToWorldPoint(new Vector3(0, 1, 0)).y, 0);
-        instFloor[1].transform.position = new Vector3(Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0, 0)).x, Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0)).y, 0);
+
     }
 
 }
